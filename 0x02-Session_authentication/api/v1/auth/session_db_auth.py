bebeada4e49dc295e2247418775eb2e87b32842a
FlintStoneObed <141341733+FlintStoneObed@users.noreply.github.com>
2024-09-09 09:40:18 +0000

    session_db_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,57 +1,76 @@
 #!/usr/bin/env python3
-"""Session authentication with expiration
-and storage support module for the API.
 """
-from flask import request
-from datetime import datetime, timedelta
-
+SessionAuth class defined here
+"""
+from flask import abort, request
+from api.v1.auth.session_exp_auth import SessionExpAuth
+import uuid
 from models.user_session import UserSession
-from .session_exp_auth import SessionExpAuth
+import os
+import datetime
 
 
 class SessionDBAuth(SessionExpAuth):
-    """Session authentication class with expiration and storage support.
-    """
+    """ SessionDBAuth class """
 
-    def create_session(self, user_id=None) -> str:
-        """Creates and stores a session id for the user.
-        """
-        session_id = super().create_session(user_id)
-        if type(session_id) == str:
-            kwargs = {
-                'user_id': user_id,
-                'session_id': session_id,
-            }
-            user_session = UserSession(**kwargs)
-            user_session.save()
-            return session_id
+    def create_session(self, user_id=None):
+        """ wraps SessionAuth func """
+        if user_id is None or type(user_id) != str:
+            return
+        UserSession.load_from_file()
+        user_session = UserSession()
+        user_session.user_id = user_id
+        sess_id = str(uuid.uuid4())
+        user_session.session_id = sess_id
+        user_session.save()
+        return sess_id
 
     def user_id_for_session_id(self, session_id=None):
-        """Retrieves the user id of the user associated with
-        a given session id.
-        """
-        try:
-            sessions = UserSession.search({'session_id': session_id})
-        except Exception:
-            return None
-        if len(sessions) <= 0:
-            return None
-        cur_time = datetime.now()
-        time_span = timedelta(seconds=self.session_duration)
-        exp_time = sessions[0].created_at + time_span
-        if exp_time < cur_time:
-            return None
-        return sessions[0].user_id
+        """ retreives sesion dic from user_id_by_session_id """
+        UserSession.load_from_file()
+        attr = {
+            'session_id': session_id
+        }
+        user_sessions = UserSession.search(attr)
+        user_id = None
 
-    def destroy_session(self, request=None) -> bool:
-        """Destroys an authenticated session.
-        """
-        session_id = self.session_cookie(request)
-        try:
-            sessions = UserSession.search({'session_id': session_id})
-        except Exception:
+        if user_sessions:
+            user_session = user_sessions[0]
+            user_id = user_session.user_id
+        else:
+            return None
+
+        if self.session_duration <= 0:
+            return user_id
+
+        sess_creation_time = user_session.created_at
+
+        now = datetime.datetime.now()
+        live_time = datetime.timedelta(seconds=self.session_duration)
+
+        if now > sess_creation_time + live_time:
+            user_session.remove()
+            return None
+
+        return user_id
+
+    def destroy_session(self, request=None):
+        """ destroys UserSession object """
+        if request is None:
             return False
-        if len(sessions) <= 0:
+        sess_id = self.session_cookie(request)
+        if not sess_id:
             return False
-        sessions[0].remove()
+        user_id = self.user_id_for_session_id(sess_id)
+        if not user_id:
+            return False
+
+        UserSession.load_from_file()
+        user_sessions = UserSession.search({
+            'session_id': sess_id
+        })
+        if not user_sessions:
+            return False
+        user_session = user_sessions[0]
+        user_session.remove()
         return True
